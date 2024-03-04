5817199aef359c38a8189e3af7c4d654bca6683b
Ksenia Lavrichenko <ksenia.lavrichenko@gmail.com>
2024-03-04 09:58:07 +0100

    adapted for the step 2

--- association_cnv/association-pipeline/snakefiles/variables.py
+++ association_cnv/association-pipeline/snakefiles/variables.py
@@ -5,9 +5,9 @@ resourcesdir = os.path.abspath(os.path.join(os.path.dirname(workflow.basedir), '
 ### Programs #######################################
 #Include here all programs and versions.You can run the specific program/version
 #calling it as {program_version} inside the code. E.g {plink17}
-plink = "plink"
-plink17 = "/home/haydee.artaza/programs/plink-1.07-x86_64/plink"
-bedtools = "/home/haydee.artaza/programs/bedtools2/bin/bedtools"
+plink = "plink2"
+plink17 = "plink"
+bedtools = "bedtools"
 
 ### Prefix file names #######################################
 ### module 1,2 and 3
@@ -26,30 +26,23 @@ cnvSNPs = "5" # bigger than 5 snps
 high_freq = "4" # 2% of 200, change these values according your strategy
 random_controls = "200"
 
-### Create paths if don't exist ###################################
-if not os.path.exists(config['log_path']):
-    os.makedirs(config['log_path'])
-if not os.path.exists(config['data_conversion_path']):
-    os.makedirs(config['data_conversion_path'])
-if not os.path.exists(config['burden_analysis_path']):
-    os.makedirs(config['burden_analysis_path'])
-if not os.path.exists(config['burden_temp_path']):
-    os.makedirs(config['burden_temp_path'])
-if not os.path.exists(config['burden_graph_path']):
-    os.makedirs(config['burden_graph_path'])
-if not os.path.exists(config['rare_cnvs_path']):
-    os.makedirs(config['rare_cnvs_path'])
-if not os.path.exists(config['rare_cnvs_summary_path']):
-    os.makedirs(config['rare_cnvs_summary_path'])
-if not os.path.exists(config['rare_cnvs_reference_path']):
-    os.makedirs(config['rare_cnvs_reference_path'])
-if not os.path.exists(config['rare_cnvs_forplots_path']):
-    os.makedirs(config['rare_cnvs_forplots_path'])
-if not os.path.exists(config['rare_cnvs_graph_path']):
-    os.makedirs(config['rare_cnvs_graph_path'])
-if not os.path.exists(config['enrichment_rare_cnvs_path']):
-    os.makedirs(config['enrichment_rare_cnvs_path'])
-if not os.path.exists(config['enrichment_rare_cnvs_genic_path']):
-    os.makedirs(config['enrichment_rare_cnvs_genic_path'])
-if not os.path.exists(config['enrichment_rare_cnvs_pathway_path']):
-    os.makedirs(config['enrichment_rare_cnvs_pathway_path'])
+### Create paths if they don't exist ###################################
+paths = [
+    config['log_path'],
+    config['data_conversion_path'],
+    config['burden_analysis_path'],
+    config['burden_temp_path'],
+    config['burden_graph_path'],
+    config['rare_cnvs_path'],
+    config['rare_cnvs_summary_path'],
+    config['rare_cnvs_reference_path'],
+    config['rare_cnvs_forplots_path'],
+    config['rare_cnvs_graph_path'],
+    config['enrichment_rare_cnvs_path'],
+    config['enrichment_rare_cnvs_genic_path'],
+    config['enrichment_rare_cnvs_pathway_path']
+]
+
+for path in paths:
+    if not os.path.exists(path):
+        os.makedirs(path)
